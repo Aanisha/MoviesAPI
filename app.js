f5e9bbb662875d6246fd7fb85f29ffffbc5b3edf
Aanisha Bhattacharyya <43934116+Aanisha@users.noreply.github.com>
2020-05-01 17:28:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const cors=require('cors');
 
-app.listen(process.env.PORT|8000, () => console.log("Server Start at 5000 Port"));
+app.listen(process.env.PORT, () => console.log("Server Start at 5000 Port"));
 
 app.use(express.static('public'));
 app.use(cors());
